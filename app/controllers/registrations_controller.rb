98d66588245b851828dd5a9ae142d9cda939a349
Alex Hower <alex.c.hower@gmail.com>
2017-06-14 21:52:50 -0400

    Added recaptcha to devise user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+   if !verify_recaptcha
+      flash.delete :recaptcha_error
+      build_resource(sign_up_params)
+      resource.valid?
+      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render :new }
+    else
+      flash.delete :recaptcha_error
+      super
+    end
+  end
+
   private
 
   def sign_up_params
