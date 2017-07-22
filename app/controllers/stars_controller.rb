adce8feaf54207faa8df51bbac7f3fe83917aed5
Alex Hower <alex.c.hower@gmail.com>
2017-07-22 16:50:05 -0400

    still working on fixing starring

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -5,7 +5,7 @@ class StarsController < ApplicationController
     @user = current_user
     @restaurant = Restaurant.find(params[:restaurant_id])
     @star = current_user.stars.create(restaurant: @restaurant)
-    redirect_to restaurants_path, notice: "Restaurant starred"
+#    redirect_to restaurants_path, notice: "Restaurant starred"
   end
 
   def unstar
@@ -13,6 +13,6 @@ class StarsController < ApplicationController
     @star = @user.stars.find_by_restaurant_id(params[:restaurant_id])
     @restaurant = Restaurant.find(params[:restaurant_id])
     @star.destroy!
-    redirect_to restaurants_path, notice: "Restaurant unstarred"
+    # redirect_to restaurants_path, notice: "Restaurant unstarred"
   end
 end
